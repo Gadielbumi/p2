621298e7a3eba1f695ae2b267b32dd0a5bbce56a
airisu <airisukun2@gmail.com>
2026-01-23 22:49:54 +0200

    Implement sun button functionality and update background styles for improved UI

--- browser-extensions-manager-ui-main/basics.js
+++ browser-extensions-manager-ui-main/basics.js
@@ -3,3 +3,24 @@
 // sun.addEventListener("click",() => {
 //     alert("ca fonctionne ")
 // })
+function changementState(){
+    const src1 = "./assets/images/icon-moon.svg" 
+    const src2 = "./assets/images/icon-sun.svg"
+    let toggle =  true
+    const button = document.getElementById("sun")
+    const img = document.getElementById("state")
+    button.addEventListener("click", ()=>{
+        if (toggle){
+            img.src = src2
+            document.body.style.backgroundColor = "#0a1a3a"
+            document.header.style.backgroundColor ="#708090"
+            toggle = !toggle
+        }
+        else{
+            img.src = src1
+            document.body.style.backgroundColor = "#ffff";
+            toggle = !toggle
+        }     
+    })
+}
+changementState()
